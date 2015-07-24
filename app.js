c3d0a7c2394fe2327c3153000916d669b5fb4370
LisaALarson <lisalarsonll@gmail.com>
2015-07-24 11:29:38 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// console.log("js works!");
+
+$(document).ready(function(){
+
+	// console.log("document ready works");
+
+	$.ajax('data.json', {
+        success: function(response){
+        	$.each(response, function(index, response){
+	            var msg = $("<p></p>").css("background-color", response.color);
+	            msg.append("Color: " + response.color);
+	            msg.append("<br>Value: " + response.value);
+	            $('.myContainer').append(msg);
+	        });
+        },
+        error: function(request, errorType, errorMessage){
+            alert(errorType + " " + errorMessage);
+        }
+    });
+
+});//end document ready
+
+//Type this into terminal AT THE FOLDER YOU ARE ACCESSING if you need to localize server --> python -m SimpleHTTPServer 8080
+//THEN open a new tab in chrome and navigate to http://127.0.0.1:8080
+
